9f36fb3623690ec53ed7c6369b844071c00d6266
aleczee <alec.aextecki@gmail.com>
2025-08-12 21:11:52 +0800

    August16 Assignment 1

---
+++ SmallestNumber.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class SmallestNumber {
+        static int small(int a, int b, int c) {
+            if (a < b && a < c) { // check if && or both is true 
+                return a;
+            } else if (b < a && b < c) {
+                return b;
+            } else {
+                return c;
+            }
+        }
+
+        public static void main(String[] args) {
+            int a = 0, b = 0, c = 0; // input numbers
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter three numbers: ");
+            a = sc.nextInt();
+            b = sc.nextInt();
+            c = sc.nextInt();
+            sc.close();
+            System.out.println("The smallest number is: " + small(a, b, c));
+        }   
+}
